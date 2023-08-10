300a133d3078b688c4ed042ba153fa2acab13ff7
khurrams4 <khurram.shahzad@itcurves.com>
2023-08-10 19:47:58 +0500

    auth/onboarding/page chandes

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -21,12 +21,12 @@ export default async function Page() {
       <p className="mt-3 text-base-regular text-light-2">
         complete your profile now to use Threads
       </p>
-      {/* <section className="mt-9 bg-dark-2 p-10">
-        <AccountProfile
+      <section className="mt-9 bg-dark-2 p-10">
+        {/* <AccountProfile
            user={userData}
            btnTitle="Continue"
-        />
-      </section> */}
+        /> */}
+      </section>
       </main>
     )
   }
