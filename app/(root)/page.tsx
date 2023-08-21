dfd2e8dc4c677c76c6b94e0a57565627e49452a6
khurrams4 <khurram.shahzad@itcurves.com>
2023-08-21 22:14:40 +0500

    thread caed created and render smoothly

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,40 +1,35 @@
-//app/page.tsx
-"use client"
 
+import Threadcard from "@/components/cards/Threadcard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
 
 export default async function Home() {
          const result = await fetchPosts(1,30);
-         
+         const user= await currentUser();
+         //console.log(result);
   return (
     <>
-      <h1 className="head-text text-light-2 text-left"> 
-        home
-      </h1>
+      <h1 className="head-text text-light-2 text-left"> home </h1>
       <section className="mt-9 flex flex-col gap-10">
-        {
-            result.posts.length === 0 ?
-            (
+        { result.posts.length === 0 ? (
             <p className="no-result"> No Threads found </p>
-            ):
-            (
+            ):(
               <>
                   {result.posts.map((post)=>(
                       <Threadcard
                       key={post._id}
                       id={ post._id}
-                      currentUserId={user?.id}
+                      currentUserId={user?.id||""}
                       parentId={post.parentId}
                       content={post.text}
                       author={post.author}
                       community={post.community}
                       createdAt={post.createdAt}
-                      comments={post.childder}
+                      comments={post.children}
                       />
                   ))}
-            </>
+               </>
             )
         
         }
