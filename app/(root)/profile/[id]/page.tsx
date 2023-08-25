99ea798f8a46f1a072bde7bcf87fc1ecdc276843
khurrams4 <khurram.shahzad@itcurves.com>
2023-08-25 20:37:25 +0500

    profile tabs added with shred

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,11 @@
 
 import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 async function Page({params}:{params:{id:string}}){
     const user= await currentUser();
@@ -20,6 +23,22 @@ async function Page({params}:{params:{id:string}}){
              bio ={userInfo.bio}
            
            />
+
+<div className="mt-9">
+                <Tabs defaultValue="thread" className="w-full">
+                    <TabsList className="tab">
+                        {profileTabs.map((tab)=>(
+                            <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                                <Image src={tab.icon} alt={tab.label}  width={24} height={24}
+                                className="object-contain" />
+                                <p className="max-sm:hidden"> {tab.label}</p>
+
+                            </TabsTrigger>
+                        ))}
+
+                    </TabsList>
+                </Tabs>
+           </div>
         </section>
     )
 }
