cea66c4d24c8df6d2e9197f5953945fe315e0f63
khurrams4 <khurram.shahzad@itcurves.com>
2023-08-28 22:54:59 +0500

    search user card  finished

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,59 @@
+
+
+
+
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import UserCard from "@/components/cards/UserCard";
+
+async function Page(){
+    const user= await currentUser();
+    if(!user) return null;
+    const userInfo= await fetchUser(user.id)
+    if(!userInfo?.onboarded) redirect('/onboading');
+
+    //Fetch users
+
+    const result =  await fetchUsers({
+        userId: user.id,
+        searchString:'',
+        pageNumber:1,
+        pageSize:25,
+    })
+    return (
+             <section>
+                <h1 className="head-text mb-10">     Search Now    </h1> 
+
+                {/* Search Bar placed */}
+                <div className="mt-14 flex flex-col gap-9">
+                    {
+                     result.users.length=== 0 ?
+                       (<p className="no-result"> No users</p>):
+                       (
+                         <>
+                        { result.users.map((person) => (
+                            <UserCard 
+                              key = {person.id}
+                              id= {person.id}
+                              name={person.name}
+                              username={person.username}
+                              imgUrl = {person.image}
+                              personType='User'
+
+                              />
+                         ))}
+                         </>
+                       )
+                    }
+
+                </div>
+             </section>
+           )
+}
+
+export default Page;
