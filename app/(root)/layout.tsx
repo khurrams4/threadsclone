e22fbe88224c0c3e3da3a38fcdac2e75cc14fff0
khurrams4 <khurram.shahzad@itcurves.com>
2023-09-07 17:38:51 +0500

    final changes...

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,48 +1,49 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
+const inter = Inter({ subsets: ["latin"] });
 
-
-import '../globals.css'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import TopBar from '@/components/shared/TopBar'
-import LeftSideBar from '@/components/shared/LeftSideBar'
-import RightSideBar from '@/components/shared/RightSideBar'
-import BottomBar from '@/components/shared/BottomBar'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata= {
-  title: 'THREADS',
-  description: 'Next.js 13 metadata Threads Application',
-}
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className} bg-dark-1`}>
-        <TopBar/>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
 
-        <main className='flex flex-row'>
-          <LeftSideBar/>
-          <section className="main-container">
-            <div className='="w-full max-w-4xl'>
-                {children}
-            </div>
-          </section>
-        
-        <RightSideBar/>
-        </main>
-        
-        <BottomBar/>
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
         </body>
-    </html>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
-
